5d9b351b1d273486f51f0b2e11c2701b2c949fa1
mateusz-kowalski <44724851+mateusz-kowalski@users.noreply.github.com>
2019-11-27 14:29:00 +0000

    Checking is it working

--- StudentLife/index.php
+++ StudentLife/index.php
@@ -30,7 +30,7 @@ and open the template in the editor.
                     <hr align="left" >
                     <p>HELLO THERE Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+                    <p>checking is it working</p>
                     <button class="btn btn-light" href="#" style="float: right;"> Get Started</button>
 
                 </div>
