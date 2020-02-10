f5695d2ef217099efeb0e4a45b7466433f096f96
Shauna Goodman <shaunafitz0402@gmail.com>
2020-02-10 14:54:53 +0000

    Search Functionality
    search bar working, db config working and diet restrictions and intolerances working

--- StudentLife/recipes-list.php
+++ StudentLife/recipes-list.php
@@ -12,17 +12,7 @@ and open the template in the editor.
 
         <?php session_start();
         include_once 'includes/CDNs.php'; 
-        $servername = "localhost";
-        $username = "student_user";
-        $password = "";
-        $db_name = "Student_db";
-        
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $db_name);
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
+        require_once 'includes/database/connection.php';
         ?> 
         <link href="includes/stylesheet.css" rel="stylesheet" type="text/css"/>
         <script src="javascript/addInput.js"></script>
@@ -32,40 +22,25 @@ and open the template in the editor.
 
 <div id="recipe-list" >
     <?php
-        $query = "SELECT * FROM recipes";
-        $result = $conn->query($query);
-        if ($result->num_rows > 0) {
-            // output data of each row
-            $name = "";
-            $difficulty = ""; 
-            $maxTime = "";
-           // $image_name = "";
-            while($row = $result->fetch_assoc()) {
-                $name = $row["name"];
-                $difficulty = $row["difficulty"];
-                $servings = $row["servings"];
-                $image_name = $row["image"];
-                $maxTime = $row["maxTime"];
+    $sql = 'SELECT * FROM recipes';
+    echo "<div class = 'recipe-details'>";
+    foreach ($conn->query($sql) as $row) {
+        echo $row['name'] . "</br>";
+        echo "Difficulty: " . $row['difficulty'] . "</br>";
+        echo "Serves: " . $row['servings'] . "</br>";
+        $maxTime = $row["maxTime"];
                 $time = explode(':', $maxTime);
                 $minutes = ($time[0] * 60.0 + $time[1] * 1.0);
-                echo "<div class = 'recipe-image'>";
-                echo "IMAGE"; //THIS IS WHERE THE IMAGE WILL BE DISPLAYING.
-                echo "</div>";
-
-                echo "<div class = 'recipe-details'>";
-                echo $name . "</br>";
-                echo "Difficulty: " . $difficulty . "</br>";
-                echo "Serving: " .$servings . "</br>";
-                echo "Time: " .$minutes . " minutes" ."</br>";
-                echo "</div>";
-        }
-        }
+                echo $minutes . " minutes</br>";
+                echo "<href = ''>Link to recipe</a> </br>";
+    }
+    echo "</div>";
     ?>
 </div>
 
 <?php
 include_once 'includes/footer.php';
-$conn->close();
+$conn = null; 
 ?>
 </body>
 </html>
