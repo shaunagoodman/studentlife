e27907688847365532c44987eeb71fcd429a0727
Niamh Curran <niamhcurran@live.ie>
2020-02-28 00:20:53 +0000

    Styled Single Recipe/API
    More styling
    Single recipe page has fake comment section for placement purposes

--- StudentLife/recipe-api.php
+++ StudentLife/recipe-api.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 include_once 'includes/database/connection.php';
@@ -30,93 +29,150 @@ include_once 'includes/CDNs.php';
   <div class="container">
 
     <br>
-    <center> <h1>What's in your fridge?</h1> </center>
-      <hr class="faq-line" >
-      <p>Can't decide what to make? Just enter below what food you have at home and we'll give you some delicious 
-        recipes that you can make with them!</p>
+    <center>
+      <h1>What's in your fridge?</h1>
+    </center>
+    <hr class="faq-line">
+    <p>Can't decide what to make? Just enter below what food you have at home and we'll give you some delicious
+      recipes that you can make with them!</p>
 
-    <div class="col-md-6 recipe-api" > <!--Search Area-->
+    <div class="col-md-6 recipe-api">
+      <!--Search Area-->
       <!--Search Ingredients-->
-      <div id = "ingredientInput">
-        <input class="form-control api-form-control" name='ingredients' id='ingredients'  placeholder="eggs, milk, butter" />
-        <input type="checkbox" id="addTime" onClick = "toggleTime()" />Select if you wish to add a time limit (If you do not set a limit it will defualt to 10 mins).<br>
-        <div id = "addedTime">
-       <p> Enter how many minutes long you want to spend cooking </p>
-        <input class="form-control api-form-control" name='time' id='time'/>
-</div>
-        <input type="checkbox" id="selectIntolerance" onClick = "toggleIntolerances()" />Select if you have an Intolerance<br>
+      <div id="ingredientInput">
+        <input class="form-control api-form-control" name='ingredients' id='ingredients' placeholder="eggs, milk, butter" /> <br>
+        <p> <input class='checkbox-api' type="checkbox" id="addTime" onClick="toggleTime()" />Select if you wish to add a time limit (If you do not set a limit it will defualt to 10 mins).</p>
+        <div id="addedTime">
+          <p> Enter how many minutes long you want to spend cooking </p>
+          <input class="form-control api-form-control" name='time' id='time' />
+        </div>
+        <br>
 
-        <div id = "intoleranceList" >
-          <p>Select any of the following intolerances: </p></br>
-          <input type="checkbox" name="intolerance" value="dairy">Dairy<br>
-          <input type="checkbox" name="intolerance" value="egg">Egg<br>
-          <input type="checkbox" name="intolerance" value="gluten">Gluten<br>
-          <input type="checkbox" name="intolerance" value="grain">Grain<br>
-          <input type="checkbox" name="intolerance" value="peanut">Peanut<br>
-          <input type="checkbox" name="intolerance" value="seafood">Seafood<br>
-          <input type="checkbox" name="intolerance" value="sesame">Sesame<br>
-          <input type="checkbox" name="intolerance" value="shellfish">Shellfish<br>
-          <input type="checkbox" name="intolerance" value="soy">Soy<br>
-          <input type="checkbox" name="intolerance" value="sulfite">Sulfite<br>
-          <input type="checkbox" name="intolerance" value="tree nut">Tree Nut<br>
-          <input type="checkbox" name="intolerance" value="wheat">Wheat<br>
-        </div> 
-        <input type="checkbox" id="selectDietRestriction" onClick = "toggleDietRestrictions()" />Select if you have a Diet Restriction<br>
-        <div id = "dietRestrictionsList" >
-        <p>Select any of the following Diet Restrictions:</p>
-        <input type="checkbox" name="dietRestriction" value="gluten free">Gluten Free<br>
-        <input type="checkbox" name="dietRestriction" value="ketogenic">Ketogenic<br>
-        <input type="checkbox" name="dietRestriction" value="paleo">Paleo<br>
-        <input type="checkbox" name="dietRestriction" value="pescetarian">Pescetarian<br>
-        <input type="checkbox" name="dietRestriction" value="primal">Primal<br>
-        <input type="checkbox" name="dietRestriction" value="vegan">Vegan<br>
-        <input type="checkbox" name="dietRestriction" value="vegetarian">Vegetarian<br>
-        <input type="checkbox" name="dietRestriction" value="whole30">Whole30<br>
+        <!-- -------------------------------------------- -->
+
+        <div class='row'>
+
+          <div class='col-lg-6'>
+            <p class='sub-head-api'> <input class='checkbox-api' type="checkbox" id="selectIntolerance" onClick="toggleIntolerances()" />Select if you have an Intolerance</p>
+            <hr align="left" class="api-line">
+            <div id="intoleranceList">
+              <p><strong>Select any of the following intolerances:</strong> </p>
+              <p><input class='checkbox-api' type="checkbox" name="intolerance" value="dairy">Dairy<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="egg">Egg<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="gluten">Gluten<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="grain">Grain<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="peanut">Peanut<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="seafood">Seafood<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="sesame">Sesame<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="shellfish">Shellfish<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="soy">Soy<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="sulfite">Sulfite<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="tree nut">Tree Nut<br>
+                <input class='checkbox-api' type="checkbox" name="intolerance" value="wheat">Wheat</p>
+            </div>
+          </div>
+
+          <!-- ------------------------- -->
+
+          <div class='col-lg-6'>
+
+            <p class='sub-head-api'> <input class='checkbox-api' type="checkbox" id="selectDietRestriction" onClick="toggleDietRestrictions()" />Select if you have a Diet Restriction</p>
+            <hr align="left" class="api-line">
+            <div id="dietRestrictionsList">
+              <p><strong>Select any of the following Diet Restrictions:</strong></p>
+              <p> <input class='checkbox-api' type="checkbox" name="dietRestriction" value="gluten free">Gluten Free<br>
+                <input class='checkbox-api' type="checkbox" name="dietRestriction" value="ketogenic">Ketogenic<br>
+                <input class='checkbox-api' type="checkbox" name="dietRestriction" value="paleo">Paleo<br>
+                <input class='checkbox-api' type="checkbox" name="dietRestriction" value="pescetarian">Pescetarian<br>
+                <input class='checkbox-api' type="checkbox" name="dietRestriction" value="primal">Primal<br>
+                <input class='checkbox-api' type="checkbox" name="dietRestriction" value="vegan">Vegan<br>
+                <input class='checkbox-api' type="checkbox" name="dietRestriction" value="vegetarian">Vegetarian<br>
+                <input class='checkbox-api' type="checkbox" name="dietRestriction" value="whole30">Whole30</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- -------------------------------------------- -->
+
+        <button class="btn api-button" type="button" onclick="findRecipe()">Submit</button> <br>
+        <br>
+        <!--Choose Recipe-->
+        <div id="result" class="hide">
+          <select class="form-control api-form-control" name="selectIngredients" id="selectIngredients">
+            <option>Choose a recipe</option>
+          </select>
+          <button class="btn api-button" onClick="viewRecipe()"> View Recipe</button>
+        </div>
+        <br>
       </div>
-      <button class="btn api-button" type = "button" onclick="findRecipe()">Submit</button>
-      <!--Choose Recipe-->
-      <div id="result" class = "hide">
-        <select class="form-control api-form-control" name = "selectIngredients" id="selectIngredients">
-          <option>Choose a recipe</option>
-        </select>
-        <button class="btn api-button" onClick="viewRecipe()"> View Recipe</button>
-      </div>
-    </div>
-      <?php 
+      <?php
       $title = $servings = "";
       $ingredients = [];
       $unit = [];
       $amount = [];
       $method = [];
-      
 
-        ?>
-    <!-- ################# Recipe Displayed Below ################# -->
-    <div id = "displayedRecipe" class = "hide">
-      <h3 class = 'resultHeading'> Title </h3> </br>
-      <div id = "recipeName"></div>
-      <h3 class = 'resultHeading'> Servings </h3> </br>
-      <div id = "servings"></div>
-      <h3 class = 'resultHeading'> Equipment </h3> </br>
-      <div id = "equipment"></div>
-      <h3 class = 'resultHeading'> Cuisine </h3> </br>
-      <div id = "cuisine"></div>
-      <h3 class = 'resultHeading'> Ingredients </h3> </br>
-      <div id = "ingredientList"></div>
-      <h3 class = 'resultHeading'> Method </h3> </br>
-      <ol id = "methodList"></ol>
-      <div class="col-md-6 recipe-api" > <!--Search Area-->
-    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
-    <input type="submit" name="btnFav" value="Submit"/>
-    </form>
-    <?php include_once 'includes/database/APItoDatabase.php'; ?>
-        <!-- <button type = "submit" name = "favouriteButton">Add to Favourites </button> -->
+
+      ?>
+      <!-- ################# Recipe Displayed Below ################# -->
+      <div id="displayedRecipe" class="hide row">
+        <!-- <div class='row'> -->
+
+          <div class='col-lg-4 api-recipe'>
+            <h5 class='resultHeading'> Title </h5> 
+            <div id="recipeName"></div>
+          </div>
+
+          <div class='col-lg-4 api-recipe'>
+            <h5 class='resultHeading'> Serves </h5>
+            <div id="servings"></div>
+          </div>
+
+          <div class='col-lg-4 api-recipe'>
+            <h5 class='resultHeading'> Cuisine </h5>
+            <div id="cuisine"></div>
+          </div>
+
+          <div class='col-lg-7 api-recipe'>
+            <h5 class='resultHeading'> Method </h5> 
+            <ol id="methodList"style='padding-left: 4.5%;'></ol>
+          </div>
+
+          <div class='col-lg-5 api-recipe'>
+            <h5 class='resultHeading'> Ingredients </h5> 
+           <!-- <ul class='ingredients-list' > <div id="ingredientList"></div></ul> -->
+           <ol id="ingredientList" style='padding-left: 4.5%;'></ol>
+          </div>
+
+          <div class='col-lg-6 api-recipe'>
+            <h5 class='resultHeading'> Equipment </h5> 
+            <div id="equipment"></div>
+          </div>
+
+          
+
+        <!-- </div> -->
+
+
+
+
+
+
+        <div class="col-md-6 recipe-api">
+          <!--Search Area-->
+          <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+            <input type="submit" name="btnFav" value="Submit" />
+          </form>
+
+          <?php include_once 'includes/database/APItoDatabase.php'; ?>
+          <!-- <button type = "submit" name = "favouriteButton">Add to Favourites </button> -->
+        </div>
+        <!--End of recipe display-->
+      </div>
     </div>
-    <!--End of recipe display-->
-</div>
-  </div> <!--End recipe container-->
-</div>
-<?php
+    <!--End recipe container-->
+  </div>
+  <?php
   include_once 'includes/footer.php';
   ?>
 
--- StudentLife/recipe_single.php
+++ StudentLife/recipe_single.php
@@ -66,17 +66,103 @@ and open the template in the editor.
 
 
 
-            <h2 class="heading"><?php echo $recipe['name'] ?> </h2>
+            <h2 class="heading allRecipes-h1"><?php echo $recipe['name'] ?> </h2>
+            <hr align="left">
 
             <div class=row>
-                <div class='col-md-4'>
-                    <img src='images/recipes/<?php echo $recipe['image'];  ?>' alt='dish image' height='250' width='270'>
+                <div class='col-md-5 single-recipe-topRow'>
+                    <img class='single-recipe-pic' src='images/recipes/<?php echo $recipe['image'];  ?>' alt='dish image'>
                 </div>
 
-                <div class='col-md-8'>
-                    <iframe width="600" height="338" src="https://www.youtube.com/embed/<?php echo $recipe["video_name"] ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class='col-md-7 single-recipe-topRow'>
+                    <p><img src='images/recipeasy-icons-logos/gauge.png' style='margin-right:1.5%' alt='clock icon' height='35' width='35'><strong>Difficulty: </strong><?php echo $difficulty ?>
+                        <img src='images/recipeasy-icons-logos/knife-fork.png' style='margin-right:1.5%' alt='clock icon' height='35' width='35'><strong>Servings:</strong> <?php echo $recipe['servings'] ?>
+                        <img src='images/recipeasy-icons-logos/clock.png' style='margin-right:1.5%' alt='clock icon' height='30' width='30'><strong>Cooking Time: </strong><?php echo $recipe['maxTime'] ?></p>
 
-                    </video>
+                    <h5> <strong>Ingredients: </strong></h5>
+                    <hr align="left" class="single-recipe-line-ingredients">
+                    <?php foreach ($recipes as $recipe) :
+                        $queryrecipeings = 'SELECT * FROM recipeingredient WHERE recipe_ID=:recipe_ID';
+                        $statement4 = $conn->prepare($queryrecipeings);
+                        $statement4->bindValue(':recipe_ID', $recipe["recipe_ID"]);
+                        $statement4->execute();
+                        $recipeings = $statement4->fetchAll();
+                        $statement4->closeCursor();
+                    ?>
+
+                        <?php foreach ($recipeings as $recipeing) :
+                            $querydesc = 'SELECT * FROM ingredients WHERE ingredient_ID=:ingredient_ID';
+                            $statement6 = $conn->prepare($querydesc);
+                            $statement6->bindValue(':ingredient_ID', $recipeing["ingredient_ID"]);
+                            $statement6->execute();
+                            $ingredients = $statement6->fetchAll();
+                            $statement6->closeCursor();
+                        ?>
+
+                            <?php foreach ($ingredients as $ingredient) : ?>
+                                <p><?php echo $ingredient['name'] ?> <?php echo $ingredient['amount'] ?> <?php echo $ingredient['unit'] ?></p><?php endforeach; ?>
+
+                        <?php endforeach; ?>
+
+                    <?php endforeach; ?>
+
+                    <?php foreach ($steps as $step) :
+
+                        $querydesc = 'SELECT * FROM steps WHERE steps_ID=:steps_ID';
+                        $statement5 = $conn->prepare($querydesc);
+                        $statement5->bindValue(':steps_ID', $step["steps_ID"]);
+                        $statement5->execute();
+                        $descriptions = $statement5->fetchAll();
+                        $statement5->closeCursor();
+                    ?>
+
+                        <?php foreach ($descriptions as $description) : ?>
+                            <h5><strong>Method: </strong></h5>
+                            <hr align="left" class="single-recipe-line">
+                            <p><?php echo $description['description'] ?></p>
+                            <?php endforeach; ?><?php endforeach; ?>
+                </div>
+
+                <div class='col-lg-5'>
+                    <h3>Comments</h3>
+
+                    <div class='fake-comment'>
+                        <h5>Really Cool!</h5>
+                        <p>
+                            Minim sit qui ut dolore reprehenderit velit ipsum.
+                            Aute in nulla commodo velit. Voluptate duis minim nisi est enim.
+                            Laborum non ipsum laboris ea veniam ut exercitation ea voluptate
+                            adipisicing sint ut. Nisi duis reprehenderit irure labore non id cillum.
+                        </p>
+                    </div>
+                    <br>
+                    <div class='fake-comment'>
+                        <h5> Could Use salt</h5>
+                        <p>
+                            Voluptate duis minim nisi est enim.
+                            Laborum non ipsum laboris ea veniam ut exercitation ea voluptate
+                        </p>
+                    </div>
+
+                    <form>
+                        <div class="form-group">
+                            <label>Comment Here</label>
+                            <textarea class="form-control fake-textBox" name="message" rows="3" placeholder=""></textarea>
+                        </div>
+                        <button class='btn btn-light btn-sm' >Send</button>
+                    </form>
+<br>
+                </div>
+                                
+
+                <div class='col-lg-7'>
+                    <h3>Video Tutorial</h3>
+                    <center>
+                        <iframe width="600" height="338" src="https://www.youtube.com/embed/<?php echo $recipe["video_name"] ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                        </video>
+
+                    </center>
                 </div>
 
             </div>
@@ -87,49 +173,7 @@ and open the template in the editor.
 
 
 
-            <p><strong>Difficulty: </strong><?php echo $difficulty ?></p>
-            <p><strong>Servings:</strong> <?php echo $recipe['servings'] ?></p>
 
-            <p><strong>Cooking Time: </strong><?php echo $recipe['maxTime'] ?></p>
-
-            <p> <strong>Ingredients: </strong></p>
-
-            <?php foreach ($recipes as $recipe) :
-                $queryrecipeings = 'SELECT * FROM recipeingredient WHERE recipe_ID=:recipe_ID';
-                $statement4 = $conn->prepare($queryrecipeings);
-                $statement4->bindValue(':recipe_ID', $recipe["recipe_ID"]);
-                $statement4->execute();
-                $recipeings = $statement4->fetchAll();
-                $statement4->closeCursor();
-            ?>
-
-                <?php foreach ($recipeings as $recipeing) :
-                    $querydesc = 'SELECT * FROM ingredients WHERE ingredient_ID=:ingredient_ID';
-                    $statement6 = $conn->prepare($querydesc);
-                    $statement6->bindValue(':ingredient_ID', $recipeing["ingredient_ID"]);
-                    $statement6->execute();
-                    $ingredients = $statement6->fetchAll();
-                    $statement6->closeCursor();
-                ?>
-
-
-                    <?php foreach ($ingredients as $ingredient) : ?>
-                        <p><?php echo $ingredient['name'] ?> <?php echo $ingredient['amount'] ?> <?php echo $ingredient['unit'] ?></p><?php endforeach; ?>
-                <?php endforeach; ?>
-            <?php endforeach; ?>
-
-            <?php foreach ($steps as $step) :
-
-                $querydesc = 'SELECT * FROM steps WHERE steps_ID=:steps_ID';
-                $statement5 = $conn->prepare($querydesc);
-                $statement5->bindValue(':steps_ID', $step["steps_ID"]);
-                $statement5->execute();
-                $descriptions = $statement5->fetchAll();
-                $statement5->closeCursor();
-            ?>
-                <?php foreach ($descriptions as $description) : ?>
-
-                    <p><strong>Method: </strong><?php echo $description['description'] ?></p><?php endforeach; ?><?php endforeach; ?>
     </div>
 
 
