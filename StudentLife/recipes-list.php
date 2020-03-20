a0698e1f336fe6a19f4822d989ad7c9a0d548f11
Shauna Goodman <shaunafitz0402@gmail.com>
2020-03-20 21:58:18 +0000

    More changes

--- StudentLife/recipes-list.php
+++ StudentLife/recipes-list.php
@@ -2,109 +2,99 @@
 // Initialize the session
 session_start();
 require_once 'includes/database/connection.php';
-
 try {
-    //write a query to get the dynamic categories (All categories)  maybe go to phpmyadmin to check is it working
-    $query = "SELECT * from difficulties";           //run in phpmyadmin to check if working
-
-    //run my query 
-    //prepare the query (PDO)
+    //Difficuties
+    $query = "SELECT * from difficulties";
     $statement = $conn->prepare($query);
-
-    //bind data if required  (if i need to contrain using a WHERE clause) not needed rn cos we need all categories
-    //execute the query
     $statement->execute();
-
-
-    //create a variable to save the result set ($categories)
     $difficulties = $statement->fetchAll();
-    //close the statement
+    $statement->closeCursor();
+
+    //Cuisines
+    $query = "SELECT * from cuisine ORDER BY name";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $cuisine = $statement->fetchAll();
     $statement->closeCursor();
 
     if(isset($_POST['submit'])) {
         $searchString = $_POST['something'];
         $array = explode(" ",$searchString);
-        if(!empty($array))
-        {
-            $sql = "SELECT * FROM recipes WHERE name ";
-            $count = 0;
-            forEach($array as $value) {
-                if ($count == 0) {
-                    $sql .= " LIKE '%" . $value . "%'";
-                }
-                else {
-                    $sql .= " OR '%" . $value . "%'";
-                }
-                $count++;
+        $sql = "SELECT * FROM recipes WHERE name ";
+        $count = 0;
+        forEach($array as $value) {
+            if ($count == 0) {
+                $sql .= " LIKE '%" . $value . "%'";
             }
-            $statement = $conn->prepare($sql);
-            $statement->execute();
-            $recipes = $statement->fetchAll();
+            else {
+                $sql .= " OR '%" . $value . "%'";
+            }
+            $count++;
         }
-
-        
-        
-
-}
-else if(empty($_POST['submit'])) {
-    //get the category id from the URL (if there is one)
-    $difficultyID = filter_input(INPUT_GET, "difficulty_id", FILTER_VALIDATE_INT);
-    //write a query to get the dynamic products (all products - from the categories ive selected) 
-    if ($difficultyID != "") {
-        //query if a value has been passed for category id
-        $query = "SELECT * from recipes WHERE difficultyID = :difficulty_id";
-    } else {
-        //query if a value has NOT been passed for category id
-        $query = "SELECT * from recipes";
+        $statement2 = $conn->prepare($sql);
+        $statement2->execute();
+        $recipes = $statement2->fetchAll();
+        $statement2->closeCursor();
     }
-
-    //prepare the query (PDO)
-    $statement2 = $conn->prepare($query);
-
-    //bind data if required  (if i need to contrain using a WHERE clause)
-    $statement2->bindValue(":difficulty_id", $difficultyID);
-    //execute the query
-    $statement2->execute();
-    //create a variable to save the result set ($products)
-    $recipes = $statement2->fetchAll();
-    //close the statement
-    $statement2->closeCursor();
+    else {
+        $difficultyID = filter_input(INPUT_GET, "difficulty_id", FILTER_VALIDATE_INT); 
+        $cuisineID = filter_input(INPUT_GET, "cuisine_ID", FILTER_VALIDATE_INT); 
+   if (isset($_POST["postvar"]))
+   {
+       echo $_POST["postvar"];
+   }
+        if ($difficultyID != "") {
+            $query = "SELECT * from recipes WHERE difficultyID = '$difficultyID'";
+        }
+        else if ($cuisineID != "") {
+            $query = "SELECT * FROM recipes r INNER JOIN recipecuisine rc ON r.recipe_ID = rc.recipe_ID WHERE rc.cuisine_ID = '$cuisineID'";
+        } 
+        else if(isset($_POST['time'])) {
+            $query = "SELECT * FROM recipes WHERE `maxTime`<= '00:".$_POST['range'].":00' ";
+        }  
+        else if(isset($_POST['sortAsc'])) {
+            $query = "SELECT * FROM recipes ORDER BY name ASC";
+        }  
+        else if(isset($_POST['sortDesc'])) {
+            $query = "SELECT * FROM recipes ORDER BY name DESC ";
+        } 
+        else if(isset($_POST['recent'])) {
+            $query = "SELECT * FROM recipes ORDER BY maxTime ";
+        }   
+        else if(isset($_POST['nonUser'])) {
+            $query = "SELECT * FROM `recipes` r INNER JOIN user u ON u.user_ID = r.user_ID WHERE u.u_type = 1 ";
+        } 
+        else {
+            $query = "SELECT * from recipes";
+        }
+        $statement2 = $conn->prepare($query);
+        $statement2->execute();
+        $recipes = $statement2->fetchAll();
+        $statement2->closeCursor();
+    }
 }
-
-} catch (Exception $ex) {
+ catch (Exception $ex) {
     $errorMessage = $e->getMessage();
     echo $errorMessage;
     exit();
 }
 ?>
-
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
-    <?php include_once 'includes/CDNs.php'; ?>
+    <?php include_once 'includes/CDNs.php';  ?>
+    <script src="javascript/recipesFilter.js"></script>
+    <script src="javascript/slider.js"></script>
     <link href="includes/stylesheet.css" rel="stylesheet" type="text/css" />
-
-
 </head>
-
-<body class='site' >
-    <?php include_once 'includes/nav-menu.php'; ?>
-
+<body onload = "slider()" class='site' >
+    <?php include_once 'includes/nav-menu.php';?>
     <main class='site-content' >
-
-    <div class="container">
-    
-
-
-    <div class='sub-menu' >
+        <div class="container">
+            <!-- DIFFICULTIES -->
+        <div class='sub-menu' >
         <?php
         //get the results from the categories variable(usuing a loop)
         echo "<div id='div-difficulty-list' >";
@@ -120,17 +110,52 @@ and open the template in the editor.
         echo "</ul>";
         echo "</div>";
         ?>
-
     </div>
 
+        <!-- CUISINE -->
+        <div class='sub-menu' >
+        <?php
+        //get the results from the categories variable(usuing a loop)
+        echo "<div id='div-difficulty-list' >";
+        echo "<ul class='diff-list' id='ul-difficulty-list' >";
+        foreach ($cuisine as $c) :
+            echo "<li class='li-diff-list'>";
+            echo "<a class='diff-menu-a' href='recipes-list.php?cuisine_ID=" . $c['cuisine_ID'] . "'>";
+            echo $c['name'];
+            echo "</a>";
+            echo "</li>"; 
+        endforeach;
+        echo "</ul>";
+        echo "</div>";
+        ?>
+        <form method = "POST">
+        <label>Max Time</label>
+        <div>
+        <input name = "range" type = "range" min = "1" max = "60" value = "10" id = "myRange"/>
+            <span id = "demo"> <span>
+            	
+        </div>
+        <input type = "submit" name = "time" value = "Filter by Time" />
+        <div>
+        <label> Sort Asc</label>
+        <input name = "sortAsc" type = "submit" value = "SortAsc"/>
+        </div>
+        <div>
+        <label> Sort Desc</label>
+        <input name = "sortDesc" type = "submit" value = "SortDesc"/>
+        </div>
+        <label> Most Recent </label>
+        <input name = "recent" type = "submit" value = "Recent"/>
+        </div>
+        <label> Non user Generated </label>
+        <input name = "nonUser" type = "submit" value = "nonUser"/>
+        </div>
+        </form>
 
-    <h1 class="allRecipes-h1" >All Recipes</h1>
-    <hr align="left">
-    
-
+        <h1 class="allRecipes-h1" >All Recipes</h1>
+        <hr align="left">
         <?php
         echo "<div class='row' >";
-        //get the results from the $products variable(using a loop)
         if(!empty($recipes)) {
         foreach ($recipes as $recipe) :  
         if($recipe['difficultyID'] == 1) {
@@ -158,33 +183,69 @@ and open the template in the editor.
             }     
         }
         ?>
-             <div class="col-lg-4 bottom-home ">
-                    <div class="card home-card recipe-page-card">
-                        <img src="<?php echo $src;?>" class="card-img-top" alt='dish image' height='315' width='328'>
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $recipe['name'];  ?></h5>
-                            <p class="card-text" class='recipe-difficulty'> Difficulty: <?php echo $difficulty; ?> </p>
-                            <p class="card-text" class='recipe-time'> <img src='images/recipeasy-icons-logos/clock.png' style='margin-bottom:0.3%' alt='clock icon' height='25' width='25'> Time: <?php echo $recipe['maxTime']; ?>
-                            </p>
-                            <center><a href="recipe_single.php?recipe_ID=<?php echo $recipe['recipe_ID'] ?>"><button type="button" class="btn btn-light stretched-link">View Recipe</button></a> </center>
-                        </div>
-                    </div>
+        <div class="col-lg-4 bottom-home ">
+            <div class="card home-card recipe-page-card">
+                <img src="<?php echo $src;?>" class="card-img-top" alt='dish image' height='315' width='328'>
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $recipe['name'];  ?></h5>
+                    <p class="card-text" class='recipe-difficulty'> Difficulty: <?php echo $difficulty; ?> </p>
+                    <p class="card-text" class='recipe-time'> <img src='images/recipeasy-icons-logos/clock.png' style='margin-bottom:0.3%' alt='clock icon' height='25' width='25'> Time: <?php echo $recipe['maxTime']; ?>
+                    </p>
+                    <center><a href="recipe_single.php?recipe_ID=<?php echo $recipe['recipe_ID'] ?>"><button type="button" class="btn btn-light stretched-link">View Recipe</button></a> </center>
                 </div>
+            </div>
+        </div>
 
         <?php endforeach;
-        } 
+        }
         else {
             echo "<script language = javascript>
-            swal({  title: 'Oops!',
-             text: 'It seems like we dont have ay recipes which match your search. Why not make a new search using the recipes you have?',  
-            type: 'success',    
-            showCancelButton: false,   
-            closeOnConfirm: false,   
-            confirmButtonText: 'Aceptar', 
-            showLoaderOnConfirm: true, }).then(function() {
-                window.location = 'recipe-api.php';
-            });;
-        </script>";
+            noRecipe();
+              </script>";
+        }
+        if(isset($_POST['submit'])) {
+            $name = "";
+            $string = $_POST['something'];
+            $arraySearch = explode(" ",$string);
+            $count = count($recipes);
+            $searchString = "";
+            if($count < 9) {
+                $array = [];
+                foreach($arraySearch as $arr) {
+                    $searchString = $arr . "&";
+                }
+                $remaining = (9 - $count);
+                $string1 = "https://api.spoonacular.com/recipes/search?query=";
+                $string2 = "number=";
+                $string3 = "&apiKey=53bea2eb3c79445188bc4d3f00895d15";
+                $url = $string1 . $searchString . $string2 . $remaining . $string3;
+                $response = json_decode(file_get_contents($url),true);
+                    foreach ($response['results'] as $data) {
+                        array_push($array, $data['id']);
+                    }
+                    foreach($array as $a) {
+                        $url = "https://api.spoonacular.com/recipes/".$a."/information?apiKey=53bea2eb3c79445188bc4d3f00895d15";
+                        $response = json_decode(file_get_contents($url),true); 
+                        $name =  $response["title"];
+                        $servings =  $response["servings"];
+                        $image = $response["image"];
+                        $maxTime = $response["readyInMinutes"];
+                        ?>   
+                        <div class="col-lg-4 bottom-home ">
+                        <div class="card home-card recipe-page-card">
+                        <img src="<?php echo $image;?>" class="card-img-top" alt='dish image' height='315' width='328'>
+                        <div class="card-body">
+                        <h5 class="card-title"><?php echo$name;  ?></h5>
+                        <p class="card-text" class='recipe-difficulty'> Difficulty: No difficulty.</p>
+                        <p class="card-text" class='recipe-time'> <img src='images/recipeasy-icons-logos/clock.png' style='margin-bottom:0.3%' alt='clock icon' height='25' width='25'> Time: <?php echo $maxTime; ?> minutes
+                        </p>
+                        <center><a href="api_single.php?recipe_ID=<?php echo $a ?>"><button type="button" class="btn btn-light stretched-link">View Recipe</button></a> </center>
+                        </div>
+                        </div>
+                        </div>
+                        <?php
+                        }
+            }
         }
         echo "</div>" ;
         ?>
