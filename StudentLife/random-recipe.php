a2e60794d2f703fe7febe2bbed9df69fb6daaae6
Niamh Curran <niamhcurran@live.ie>
2020-03-26 12:14:27 +0000

    Random Recipe tidy up
    Random Recipe tidy up

--- StudentLife/random-recipe.php
+++ StudentLife/random-recipe.php
@@ -32,39 +32,41 @@ include_once 'includes/database/connection.php';
 
         <div class=row>
           <div class='col-md-7 single-recipe-topRow'>
-            
+
             <div id="image"></div>
             <form class="faveForm" action="" method="POST">
-          <!-- <input class="btn api-button random-button" type="submit" name="btnFav" value="Favourite" /> -->
-          <button id="myImage" class="myLink btn" type="submit" name="btnFav" atl="favourite me!"> </button>
-        </form>
-            
+              <!-- <input class="btn api-button random-button" type="submit" name="btnFav" value="Favourite" /> -->
+              <button id="myImage" class="myLink btn" type="submit" name="btnFav" atl="favourite me!"> </button>
+            </form>
+
           </div>
           <div class='col-md-5 single-recipe-topRow'>
-            <div class="row" >
-            <p class='random-recipe-div'>
-              <div class="col-sm-6" ><img src='images/recipeasy-icons-logos/knife-fork.png' style='margin-right:1.5%' alt='clock icon' height='35' width='35'><strong>Servings:</strong>
-              <strong id="servings"></strong></div>
-              
+            <div class="row">
+              <p class='random-recipe-div'>
+                <div class="col-sm-6 col-lg-12"><img src='images/recipeasy-icons-logos/knife-fork.png' style='margin-right:1.5%' alt='clock icon' height='35' width='35'><strong>Servings:</strong>
+                  <strong id="servings"></strong></div>
+                <br><br>
 
-              <div class="col-sm-6" ><img src='images/recipeasy-icons-logos/clock.png' style='margin-right:1.5%' alt='clock icon' height='30' width='30'><strong>Cooking Time: </strong>
-              <strong id="time"></strong></div>
-              
+                <div class="col-sm-6 col-lg-12"><img src='images/recipeasy-icons-logos/clock.png' style='margin-right:1.5%' alt='clock icon' height='30' width='30'><strong>Cooking Time: </strong>
+                  <strong id="time"></strong></div>
+                <br><br>
 
-              <div class="col-sm-6" ><img src='images/recipeasy-icons-logos/cuisine.png' style='margin-right:1.5%' alt='clock icon' height='30' width='30'><strong>Cuisine: </strong>
-              <strong id="cuisine"></strong></div>
-              
+                <div class="col-sm-6 col-lg-12"><img src='images/recipeasy-icons-logos/cuisine.png' style='margin-right:1.5%' alt='clock icon' height='30' width='30'><strong>Cuisine: </strong>
+                  <strong id="cuisine"></strong></div>
+                <br><br>
 
-              <div class="col-sm-6" ><img src='images/recipeasy-icons-logos/utensils.png' style='margin-right:1.5%' alt='clock icon' height='40' width='40'><strong>Equipment: </strong>
-              <strong id="equipment"></strong></div>
-              
+                <div class="col-sm-6 col-lg-12"><img src='images/recipeasy-icons-logos/utensils.png' style='margin-right:1.5%' alt='clock icon' height='40' width='40'><strong>Equipment: </strong>
+                  <strong id="equipment"></strong></div>
+                <br><br>
 
-            </p>
+              </p>
             </div>
 
 
 
           </div>
+
+          
 
           <div class='row ingredientMethod'>
 
@@ -75,13 +77,16 @@ include_once 'includes/database/connection.php';
                 <h5> <strong>Ingredients: </strong></h5>
               </div>
               <!-- MOBILE -->
-              <div class='ingredientsTitle'>
-                <h5> <i class=" fa2 fa fa-chevron-right" aria-hidden="true"></i>
-                  <span class='ingredientsSpan'><strong>Ingredients: </strong> </span>
+              <div class='ingredientsTitleRand'>
+                <h5><!--  <i class=" faR fa fa-chevron-right" aria-hidden="true"></i> -->
+                  <span class='ingredientsRandSpan'><strong>Ingredients: </strong> </span>
                 </h5>
               </div>
 
-              <ol id="ingredientList" style='padding-left: 4.5%;'></ol>
+              <div class='ingredientsDivRand'>
+                <ol id="ingredientList" style='padding-left: 4.5%;'></ol>
+              </div>
+
               <br><br>
             </div>
 
@@ -93,14 +98,16 @@ include_once 'includes/database/connection.php';
                 <h5> <strong>Method: </strong></h5>
               </div>
               <!-- MOBILE -->
-              <div class='methodTitle'>
-                <h5> <i class="fa3 fa fa-chevron-right" aria-hidden="true"></i>
-                  <span class='methodSpan'><strong>Method: </strong> </span>
+              <div class='methodTitleRand'>
+                 <h5><!-- <i class="faR2 fa fa-chevron-right" aria-hidden="true"></i> -->
+                  <span class='methodRandSpan'><strong>Method: </strong> </span>
                 </h5>
               </div>
 
+              <div class="methodDivRand">
+                <ol id="methodList" style='padding-left: 4.5%;'></ol>
+              </div>
 
-              <ol id="methodList" style='padding-left: 4.5%;'></ol>
 
             </div>
 
