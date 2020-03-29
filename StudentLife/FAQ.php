d98f4256044f746fd930f630855906570b852b52
OrlaithHanlon <OrlaithHanlon@users.noreply.github.com>
2020-03-29 16:01:06 +0100

    faq page

--- StudentLife/FAQ.php
+++ StudentLife/FAQ.php
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 <?php
 // Initialize the session
 session_start();
@@ -172,7 +172,7 @@ and open the template in the editor.
                             <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
                                 <div class="card-body">
                                     <p class="faq-p" > When creating your recipe you can upload a photo from your computer. Click the "choose file" button, then click the image you want to upload and click open. <br> This will upload the photo. Make sure the image is in JPG, JPEG or PNG format. <br>
-                                    <br>To add a video, you must upload your video to YouTube first. Once your video is uploaded, copy and paste the URL into the "video URL" box on the add recipe page. Make sure it is the URL and not the embbed code. <br> <br>It will look something like this: https://www.youtube.com/watch?v=Sj8CCd3nuUQ& <br><br>
+                                    To add a video, you must upload your video to YouTube first. Once your video is uploaded, copy and paste the URL into the "video URL" box on the add recipe page. Make sure it is the URL and not the embbed code. <br> <br>It will look something like this: https://www.youtube.com/watch?v=Sj8CCd3nuUQ& <br>
                                     Then once you have pasted it, and have entered all your other details for the recipe, click submit and you will see both the photo and video on the recipe page! 
                 
                                 </div>
@@ -206,151 +206,3 @@ and open the template in the editor.
 
     </body>
 </html>
-=======
-<?php
-// Initialize the session
-session_start();
-?>
-
-
-
-
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>FAQ</title>
-
-        <?php include_once 'includes/CDNs.php'; ?> 
-
-        <link href="includes/stylesheet.css" rel="stylesheet" type="text/css"/>
-
-
-
-    </head>
-    <body class='site' >
-        <?php include_once 'includes/nav-menu.php'; ?>  
-
-        <main class='site-content' >
-        <div class="container" >
-
-            <div class="row" >
-
-                
-
-                    <h1 class="faq-heading" ><span class="underline">Frequently Asked Questions</span></h1>
-                
-
-
-                <div class="col-lg-12 accordion-section ">
-
-
-                    <div id="accordion">
-                        <div class="card faq-card">
-                            <div class="card-header" id="headingOne">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link stretched-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    <i class="fas fa-search"></i> How do I find a recipe? 
-                                    </button>
-                                </h5>
-                            </div>
-
-                            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-                                <div class="card-body">
-                                    <p class="faq-p" > You can use our "What's in your fridge?" feauture to search by certain ingredients, or you can search by recipe name too!
-                                    <br> Go to the recipe section to start!</p>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <br>
-                        
-                        <div class="card faq-card">
-                            <div class="card-header" id="headingTwo">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link collapsed stretched-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    <i class="fas fa-cog"></i> How do I create an account?
-                                    </button>
-                                </h5>
-                            </div>
-                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                                <div class="card-body">
-                                   <p class="faq-p" > You can create an account by clicking on the "Login" dropdown in the menu and then clicking on 
-                                   register. Then enter your name, email and password - click "Register" and you're done!  </p>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <br>
-                        
-                        <div class="card faq-card">
-                            <div class="card-header" id="headingThree">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link collapsed stretched-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    <i class="far fa-paper-plane"></i>  I have a question! How can I contact Recipeasy?
-                                    </button>
-                                </h5>
-                            </div>
-                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                                <div class="card-body">
-                                    <p class="faq-p" > Easy! You can contact us by visiting our Contact Us page which is linked at the bottom of the website. 
-                                    Just fill out the contact form and we'll get back to you ASAP!  </p>
-                                </div>
-                            </div>
-                        </div>
-                        <br>
-
-                        <div class="card faq-card">
-                            <div class="card-header" id="headingFour">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link stretched-link" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseOne">
-                                    <i class="fas fa-utensils"></i> What is the Random Recipe Generator?
-                                    </button>
-                                </h5>
-                            </div>
-
-                            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
-                                <div class="card-body">
-                                    <p class="faq-p" > The Random Recipe Generator is a section of the website you can use when you're not sure what to cook! 
-                                    <br> It will generate a random recipe from our extensive database, and the possibilities are endless! <br>
-                                    If you see one you like, you can then add it to your favourites using the "favourite" button underneath the recipe!</p>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <br>
-                        
-                        
-                    
-                        
-                        
-                        
-                    </div>
-
-
-
-                </div>
-
-            </div>
-
-
-        </div>
-
-        </main>
-
-
-        <?php include_once 'includes/footer.php'; ?>
-
-
-
-
-
-
-    </body>
-</html>
->>>>>>> Stashed changes
